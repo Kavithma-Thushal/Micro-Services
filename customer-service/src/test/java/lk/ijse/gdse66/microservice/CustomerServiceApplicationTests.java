cc9352594a0ba29a1909fa70e39d56d4b68a8279
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-06 11:57:44 +0530

    Customer Service Added

---
+++ customer-service/src/test/java/lk/ijse/gdse66/microservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
