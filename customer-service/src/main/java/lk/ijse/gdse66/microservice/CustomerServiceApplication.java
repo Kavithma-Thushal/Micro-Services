ef14045d7a8c766a6d259c71ef4f78da43267f7b
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-06 12:43:07 +0530

    Item Service Updated

--- customer-service/src/main/java/lk/ijse/gdse66/microservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/lk/ijse/gdse66/microservice/CustomerServiceApplication.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse66.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
--- item-service/src/main/java/lk/ijse/gdse66/microservice/CustomerServiceApplication.java
+++ item-service/src/main/java/lk/ijse/gdse66/microservice/CustomerServiceApplication.java
@@ -2,12 +2,13 @@ package lk.ijse.gdse66.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerServiceApplication.class, args);
     }
-
 }
